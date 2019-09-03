77a933cfe4a1552ab0ad30a8c42576e03a99a878
lerbj88 <45861225+lerbj88@users.noreply.github.com>
2019-09-03 12:17:37 -0500

    Delete ClientesServices.java

--- src/main/java/sistema/services/ClientesServices.java
+++
@@ -1,9 +0,0 @@
-package sistema.services;
-
-import sistema.entity.Clientes;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface ClientesServices  {
-    Page<Clientes> getPages(Pageable pageable, String txt);
-}
