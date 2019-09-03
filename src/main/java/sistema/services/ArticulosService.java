1020f7e6f5980ceddb304422da9b117b5dda61f6
lerbj88 <45861225+lerbj88@users.noreply.github.com>
2019-09-03 12:17:27 -0500

    Delete ArticulosService.java

--- src/main/java/sistema/services/ArticulosService.java
+++
@@ -1,9 +0,0 @@
-package sistema.services;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import sistema.entity.Articulos;
-
-public interface ArticulosService {
-    Page<Articulos> getPages(Pageable pageable, String txt);
-}
